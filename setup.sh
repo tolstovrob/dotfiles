46f693c47f343c3cb3d3fa92ff3e06f50404e05a
tolstovrob <tolstovrob@gmail.com>
2024-11-24 21:20:15 +0400

    feat: add dotfiles setup

---
+++ setup.sh
@@ -0,0 +1,7 @@
+ln -s ~/dotfiles/nvim ~/.config/nvim
+ln -s ~/dotfiles/kitty ~/.config/kitty
+ln ~/dotfiles/.zshrc ~/.zshrc
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+source ~/.zshrc
+clear
