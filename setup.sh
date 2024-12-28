ca6f491d8d3933b3f164f045f98363796246c0a7
tolstovrob <tolstovrob@gmail.com>
2024-12-28 20:08:37 +0400

    feat: update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,8 +1,40 @@
+#!/bin/bash
+
+removePrevConfig() {
+    echo "Removing previous configuration..."
+    rm -rf ~/.zshrc
+    rm -rf ~/.p10k.zsh
+    rm -rf ~/dotfiles/kitty ~/.config/kitty
+    rm -rf ~/dotfiles/nvim ~/.config/nvim
+}
+
+# Проверка аргументов
+if [ "$#" -eq 0 ]; then
+    read -p "You already have config installed. Remove it? (y/n): " answer
+    if [[ "$answer" == "y" || "$answer" == "Y" ]]; then
+        removePrevConfig
+    else
+        echo "Cancelled"
+        exit 1
+    fi
+elif [ "$1" == "-y" ]; then
+    removePrevConfig
+fi
+
+# Создание символических ссылок
+echo "Creating symbolic links..."
 ln -s ~/dotfiles/nvim ~/.config/nvim
 ln -s ~/dotfiles/kitty ~/.config/kitty
 ln ~/dotfiles/.zshrc ~/.zshrc
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+ln ~/dotfiles/.p10k.zsh ~/.p10k.zsh
+
+# Клонирование плагинов Zsh
+echo "Cloning Zsh plugins..."
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions || echo "Failed to clone zsh-autosuggestions"
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting || echo "Failed to clone zsh-syntax-highlighting"
+
+# Перезагрузка конфигурации Zsh
 source ~/.zshrc
 
+# Очистка экрана
 clear
