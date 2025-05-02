c38c6741518fd77d750986f24ac421c36ab6bda9
tolstovrob <tolstovrob@gmail.com>
2025-05-02 22:37:39 +0400

    feat(nvim): setup plugin aliases

--- nvim/lua/keys/main.lua
+++ nvim/lua/keys/main.lua
@@ -1,3 +1,16 @@
-require('keys/alias')
+require("keys/alias")
 
-im('<C-k>', '<escape>')
+--[[ FILES ]]
+nm("<leader>w", "<CMD>update<CR>")
+nm("<leader>q", "<CMD>q<CR>")
+
+--[[ EXPLORE ]]
+nm("<leader>e", "<CMD>Neotree toggle<CR>")
+
+--[[ WINDOWS ]]
+nm("<leader>o", "<CMD>vsplit<CR>")
+nm("<leader>p", "<CMD>split<CR>")
+nm("<C-h>", "<C-w>h")
+nm("<C-l>", "<C-w>l")
+nm("<C-k>", "<C-w>k")
+nm("<C-j>", "<C-w>j")
