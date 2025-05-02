2c387dc53743570af08013e16fa04b2c368108d3
tolstovrob <tolstovrob@gmail.com>
2025-05-02 22:21:29 +0400

    refactor(nvim): move mapleader declaration to keymap file

--- nvim/lua/keys/alias.lua
+++ nvim/lua/keys/alias.lua
@@ -1,37 +1,44 @@
-local map = vim.api.nvim_set_keymap			-- keymap nvim method alias
+local map = vim.api.nvim_set_keymap -- keymap nvim method alias
+
+vim.g.mapleader = " "
+vim.g.maplocalleader = "\\"
 
 --[[
 Function to set a new keymap (normal)
 key - {string} Keymap string
 command - {string} Command
-]]--
-function nm(key, command) 
-	map('n', key, command, {noremap = true})
+]]
+--
+function nm(key, command)
+	map("n", key, command, { noremap = true })
 end
 
 --[[
 Function to set a new keymap (input)
 key - {string} Keymap string
 command - {string} Command
-]]--
+]]
+--
 function im(key, command)
-	map('i', key, command, {noremap = true})
+	map("i", key, command, { noremap = true })
 end
 
 --[[
 Function to set a new keymap (visual)
 key - {string} Keymap string
 command - {string} Command
-]]--
+]]
+--
 function vm(key, command)
-	map('v', key, command, {noremap = true})
+	map("v", key, command, { noremap = true })
 end
 
 --[[
 Function to set a new keymap (terminal)
 key - {string} Keymap string
 command - {string} Command
-]]--
+]]
+--
 function tm(key, command)
-	map('t', key, command, {noremap = true})
+	map("t", key, command, { noremap = true })
 end
