c2830a2b3b923e422930494e5c144281eb5e20ed
tolstovrob <tolstovrob@gmail.com>
2026-03-28 12:43:54 +0400

    refactor: remove nvim config

--- nvim/lua/base/search.lua
+++
@@ -1,9 +0,0 @@
-local opt = vim.opt		-- vim options variable alias
-local g = vim.g			-- vim global variables alias
-
---[[ SEARCH ]]--
-opt.ignorecase = true
-opt.smartcase = true
-opt.showmatch = true
-
-
--- nvim/lua/base/tabs.lua
+++
@@ -1,8 +0,0 @@
-local opt = vim.opt		-- vim options variable alias
-local g = vim.g			-- vim global variables alias
-
---[[ TABS ]]--
-opt.shiftwidth = 2
-opt.tabstop = 2
-opt.smartindent = true
-opt.expandtab = true
